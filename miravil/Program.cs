eae60a811f6fdad153e4c1a1535dcf80ceaa3b82
Miravil.220745 <Miravil.220745@LAB-A-PC24>
2024-02-23 08:16:21 +0800

    Add project files.

---
+++ miravil/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace miravil
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int totalgrades = 5;
+            int sum = 0;
+
+            for( int i = 0; i < totalgrades; i++ ) { 
+                Console.WriteLine("Enter 5 grades seperated by new line: ", i + 1);
+                int grades = Convert.ToInt32(Console.ReadLine());
+
+                sum += grades;
+            }
+            double result = (double)sum / totalgrades;
+            double average = Math.Round(result);
+            Console.WriteLine("The average is " + result + " and round off to " + average);
+        }
+    }
+}
